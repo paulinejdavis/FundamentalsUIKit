90ae51c0b16676caf5a313866b8d82dc08cab735
pauline <pauljdavi.t21@btinternet.com>
2024-02-07 15:28:06 +0000

    Added connection to search bar - fix bug

--- FundamentalsUIKit/ViewController.swift
+++ FundamentalsUIKit/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelOutput: UILabel!
+    @IBOutlet weak var UiSearch: UILabel!
+    @IBOutlet weak var UIButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelOutput.text = "Hi again"
     }
 
-
+    @IBAction func doButtonGo(_ sender: Any) {
+        labelOutput.text = UiSearch.text
+    }
+    
 }
 
