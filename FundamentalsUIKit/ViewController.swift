d6589c70ecd6d577cbb84116f1e536631fca725e
pauline <pauljdavi.t21@btinternet.com>
2024-02-08 15:23:01 +0000

    added in code and in storyboard example - debug and revisit

--- FundamentalsUIKit/ViewController.swift
+++ FundamentalsUIKit/ViewController.swift
@@ -13,10 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var UiSearch: UILabel!
     @IBOutlet weak var UIButton: UIButton!
     
+    let lbl = UILabel(frame: CGRect(x: 100, y: 200, width: 80, height: 40))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         labelOutput.text = "Hi again"
+        
+        lbl.text = "in code"
+        view.addSubview(lbl)
     }
 
     @IBAction func doButtonGo(_ sender: Any) {
