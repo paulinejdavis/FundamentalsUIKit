0d8491e218b17e89c012a0d0f01e4dcd9b9e39ad
pauline <pauljdavi.t21@btinternet.com>
2024-02-10 16:02:06 +0000

    Added project improvement - decimal point and currency marker

--- CalculateATip/CalculateATip/ViewController.swift
+++ CalculateATip/CalculateATip/ViewController.swift
@@ -13,23 +13,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var scTipPercentage: UISegmentedControl!
     @IBOutlet weak var lblTotal: UILabel!
     let percentages = [0.1, 0.15, 0.2, 0.25]
+    let numFormatter = NumberFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        numFormatter.numberStyle = .currency
+        
     }
 
     @IBAction func doBtnCalc(_ sender: Any) {
         calcTip()
     }
     //also do in obejectionable bonding or guard statements
+    @IBAction func doSCChanged(_ sender: Any) {
+        calcTip()
+    }
+    
+    
+    @IBAction func doTFChange(_ sender: Any) {
+        calcTip()
+        
+    }
+    
+    
     func calcTip(){
         let billAmt = Double(tfInput.text ?? "0.0") ?? 0.0
         let selectedIndex = scTipPercentage.selectedSegmentIndex
         let tipPercentage = percentages[selectedIndex]
         let tip = billAmt * tipPercentage
         let total = billAmt + tip
-        lblTotal.text = "Total: \(total)"
+        let totalStr = numFormatter.string(for: total) ?? ""
+        lblTotal.text = "Total: \(totalStr)"
     }
 }
 
